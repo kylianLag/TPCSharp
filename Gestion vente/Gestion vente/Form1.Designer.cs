4be901f50bb20f34f8d9c6fe590a8903d2fdf6df
Kylian Lagache <kylian.lagache14@gmail.com>
2024-10-02 12:39:08 +0200

    vrai projet

---
+++ Gestion vente/Gestion vente/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Gestion_vente
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ajouter = new System.Windows.Forms.Button();
+            this.modifier = new System.Windows.Forms.Button();
+            this.supprimer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(33, 64);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(449, 23);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(33, 163);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(449, 244);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // ajouter
+            // 
+            this.ajouter.Location = new System.Drawing.Point(604, 70);
+            this.ajouter.Name = "ajouter";
+            this.ajouter.Size = new System.Drawing.Size(75, 23);
+            this.ajouter.TabIndex = 2;
+            this.ajouter.Text = "ajouter";
+            this.ajouter.UseVisualStyleBackColor = true;
+            // 
+            // modifier
+            // 
+            this.modifier.Location = new System.Drawing.Point(604, 211);
+            this.modifier.Name = "modifier";
+            this.modifier.Size = new System.Drawing.Size(75, 23);
+            this.modifier.TabIndex = 3;
+            this.modifier.Text = "modifier";
+            this.modifier.UseVisualStyleBackColor = true;
+            // 
+            // supprimer
+            // 
+            this.supprimer.Location = new System.Drawing.Point(604, 144);
+            this.supprimer.Name = "supprimer";
+            this.supprimer.Size = new System.Drawing.Size(75, 23);
+            this.supprimer.TabIndex = 4;
+            this.supprimer.Text = "supprimer";
+            this.supprimer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.supprimer);
+            this.Controls.Add(this.modifier);
+            this.Controls.Add(this.ajouter);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private ListView listView1;
+        private Button ajouter;
+        private Button modifier;
+        private Button supprimer;
+    }
+}
---
+++ Gestion vente/Gestion vente/Form1.cs
@@ -0,0 +1,10 @@
+namespace Gestion_vente
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
